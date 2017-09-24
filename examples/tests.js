c2a821789959445951f9d6ca9eba94a7affb8450
Sergei Chipiga <chipiga86@gmail.com>
2017-09-24 09:44:43 +0300

    Update stdout reporter

--- examples/tests.js
+++ examples/tests.js
@@ -1,15 +1,22 @@
 "use strict";
 
-scope("My tests", () => {
-    // test("my test", () => {
-    //     chunk("my step", () => {
-    //         throw new Error("fuck world");
-    //     });
-    // });
+// scope("My tests", () => {
+//     test("my test", () => {
+//         chunk("my step", () => {
+//             throw new Error("boom");
+//         });
+//     });
 
-    test("my another test", () => {
-        chunk("my step", () => {
-            // throw new Error("fuck world");
-        });
+//     test("my another test", () => {
+//         chunk("my step", () => {
+//             throw new Error("boom");
+//         });
+//     });
+// });
+
+test("failed before", () => {
+    before(() => {
+        throw new Error("boom");
     });
+    chunk("my step", () => {});
 });
