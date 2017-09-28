41683c9141718c83997fa85b7c4e5af4814df249
Sergei Chipiga <chipiga86@gmail.com>
2017-09-28 20:34:29 +0300

    Fix typos

--- examples/tests.js
+++ examples/tests.js
@@ -1,33 +1,7 @@
 "use strict";
 
-// scope("My tests", () => {
-//     test("my test", () => {
-//         chunk("my step", () => {
-//             throw new Error("boom");
-//         });
-//     });
-
-//     test("my another test", () => {
-//         chunk("my step", () => {
-//             throw new Error("boom");
-//         });
-//     });
-// });
 scope ("My tests", () => {
-
-    // after(() => {
-    //     throw new Error("before boom");
-    // });
-
     test("failed before", () => {
-        after(() => {
-            console.log("after #1");
-            throw new Error("boom");
-        });
-        after(() => {
-            console.log('after #2');
-            // throw new Error("another boom");
-        });
         chunk("my step", () => {});
     });  
 });
