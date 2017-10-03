432f90bef716f0170c6ab616b1aeed64d90f6f7c
Sergei Chipiga <chipiga86@gmail.com>
2017-10-03 23:59:48 +0300

    fix typos

--- examples/tests.js
+++ examples/tests.js
@@ -5,6 +5,7 @@ var shouldFail = true;
 scope ("My tests", () => {
     test("failed before", () => {
         chunk("my step", async () => {
+            await SS.openUrl("https://yandex.ru");
             await SS.pause(5000, "capture video");
         });
     });  
