403c789b16603112762a712f2d68e6a2f634d6dd
Sergei Chipiga <chipiga86@gmail.com>
2017-10-04 00:54:23 +0300

    Fix page usage

--- examples/tests.js
+++ examples/tests.js
@@ -2,10 +2,16 @@
 
 var shouldFail = true;
 
+var Page = require("../lib/page");
+
+var myPage = new Page("index", "/");
+
 scope ("My tests", () => {
     test("failed before", () => {
         chunk("my step", async () => {
-            await SS.openUrl("https://yandex.ru");
+            SS.registerPages(myPage);
+            await SS.openPage("index");
+            // await SS.openUrl("https://yandex.ru");
             // await SS.pause(5000, "capture video");
         });
     });  
