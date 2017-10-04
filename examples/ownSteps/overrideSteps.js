457d389e38bcb7a589777fff30f068c8567c85ad
Sergei Chipiga <chipiga86@gmail.com>
2017-10-04 23:06:09 +0300

    Add examples of own steps

---
+++ examples/ownSteps/overrideSteps.js
@@ -0,0 +1,21 @@
+// npm test -- examples/ownSteps/overrideSteps.js --web --app https://yandex.ru
+var util = require("util");
+
+var Steps = require("../../lib").Steps;
+
+var MySteps = function () {
+    Steps.call(this);
+};
+
+util.inherits(MySteps, Steps);
+
+MySteps.prototype.openApp = async function () {
+    await Steps.prototype.openApp.call(this);
+    console.log(this._appUrl);
+};
+
+global.SS = new MySteps;
+
+test("Override steps", () => {
+    chunk(async () => await SS.openApp());
+});
