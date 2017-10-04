a738f3a81a3f6234d4a7a06b60dd88b26e67b2a1
Sergei Chipiga <chipiga86@gmail.com>
2017-10-04 07:19:41 +0300

    Connect proxy

--- examples/tests.js
+++ examples/tests.js
@@ -10,9 +10,9 @@ scope ("My tests", () => {
     test("failed before", () => {
         chunk("my step", async () => {
             SS.registerPages(myPage);
-            await SS.openPage("index");
+            await SS.openPage(myPage.name);
             // await SS.openUrl("https://yandex.ru");
-            // await SS.pause(5000, "capture video");
+            await SS.pause(2000, "capture video");
         });
     });  
 });
