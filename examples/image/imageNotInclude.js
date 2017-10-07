c16e9f29d958c390b0971ee64a1681bdaf6c0a5c
Sergei Chipiga <chipiga86@gmail.com>
2017-10-07 17:37:21 +0300

    Add examples for images inclusion

---
+++ examples/image/imageNotInclude.js
@@ -0,0 +1,25 @@
+// npm test -- examples/page/imageNotInclude.js --web --app https://yandex.ru
+
+var Steps = require("../../lib").Steps;
+var Page = require("../../lib").Page;
+
+var indexPage = new Page(
+    "index", "/",
+    { searchField: "input#text",
+      searchButton: "button.button_theme_websearch[type='submit']"});
+
+test("Page usage example", () => {
+
+    before(() => {
+        SS.registerPages(indexPage);
+    });
+
+    chunk(async () => {
+        await SS.openPage(indexPage.name);
+        var searchImage = await SS.makeScreenshot({ element: "searchButton" });
+        await SS.openUrl("https://opennet.ru");
+        var fullImage = await SS.makeScreenshot();
+        await SS.checkImagesInclusion(
+            searchImage, fullImage, { shouldBe: false });
+    });
+});
