d176fffeeb2838934f8d50f6ef16fffa35d85f51
Sergei Chipiga <chipiga86@gmail.com>
2017-10-10 22:19:57 +0300

    Add proxy options for browser dynamically

---
+++ examples/proxy/dynamicGlobalProxy.js
@@ -0,0 +1,12 @@
+// npm test -- examples/proxy/dynamicGlobalProxy.js --web --global-proxy --app https://yandex.ru
+test("Dynamic global proxy", () => {
+    chunk(async () => {
+        await SS.openApp();
+        await SS.stopGlobalProxy();
+        await SS.restartBrowser();
+        await SS.openApp();
+    });
+    after(async () => {
+        if (CONF.useGlobalProxy) await SS.startGlobalProxy();
+    });
+});
