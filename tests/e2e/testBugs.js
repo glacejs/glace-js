87e31a233fe46bf787edb4bd35d4c9b3924731f9
Sergei Chipiga <chipiga86@gmail.com>
2018-04-05 19:06:13 +0300

    release 1.9.9

---
+++ tests/e2e/testBugs.js
@@ -0,0 +1,20 @@
+"use strict";
+
+scope("Fixed Bugs", () => {
+    var i = 0;
+
+    test("https://github.com/glacejs/glace-core/issues/87", { retry: 1 }, () => {
+
+        before(async () => {
+            await SS.limitProxySpeed(1024);
+        });
+
+        after(() => {
+            i++;
+        });
+
+        chunk(() => {
+            if (!i) throw new Error("BOOM!");
+        });
+    });
+});
